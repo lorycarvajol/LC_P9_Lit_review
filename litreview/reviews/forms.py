74eccc53ed1420d88e3286016b65125dd8605260
Carvajol lory <66648964+maclory73@users.noreply.github.com>
2024-08-04 16:44:06 +0200

    response_reviews ok

--- litreview/reviews/forms.py
+++ litreview/reviews/forms.py
@@ -12,7 +12,3 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ["headline", "rating", "body"]
-
-    title = forms.CharField(max_length=128, required=True)
-    description = forms.CharField(widget=forms.Textarea, required=True)
-    image = forms.ImageField(required=False)
